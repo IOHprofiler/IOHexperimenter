c17021bbae29f75aaf5a942fbe041ca2635b872a
F. Ye <f.ye@liacs.leidenuiv.nl>
2018-12-21 09:23:45 +0100

    Update optimal of functions

--- code-experiments/src/f_MIS.c
+++ code-experiments/src/f_MIS.c
@@ -82,7 +82,7 @@ static IOHprofiler_problem_t *f_MIS_allocate(const size_t number_of_variables) {
 
     /* Compute best solution */
     /*f_MIS_evaluate(problem, problem->best_parameter, problem->best_value);*/
-    problem->best_value[0] = DBL_MAX;
+    problem->best_value[0] = number_of_variables / 2.0 + 1.0;
     return problem;
 }
 
@@ -97,7 +97,7 @@ static IOHprofiler_problem_t *f_MIS_IOHprofiler_problem_allocate(const size_t fu
                                                                      const char *problem_name_template) {
 
 
-    int *z, *sigma;
+    int *z;
     int temp,t;
     size_t i;
     double a;
@@ -105,8 +105,6 @@ static IOHprofiler_problem_t *f_MIS_IOHprofiler_problem_allocate(const size_t fu
     double *xins;
     IOHprofiler_problem_t *problem;
     z = IOHprofiler_allocate_int_vector(dimension);
-    sigma = IOHprofiler_allocate_int_vector(dimension);
-    xins = IOHprofiler_allocate_vector(dimension);
     problem = f_MIS_allocate(dimension);
 
     if(instance == 1){
@@ -116,34 +114,12 @@ static IOHprofiler_problem_t *f_MIS_IOHprofiler_problem_allocate(const size_t fu
         problem = transform_vars_xor(problem,z,0);
         problem = transform_obj_shift(problem,a);
     }
-    else if(instance > 1 && instance <= 50){
-        IOHprofiler_compute_xopt(z,rseed,dimension);
-        a = IOHprofiler_compute_fopt(function,instance + 100);
-        a = fabs(a) / 1000 * 4.8 + 0.2;
-        b = IOHprofiler_compute_fopt(function,instance);
-        problem = transform_vars_xor(problem,z,0);
-        assert(a <= 5.0 && a >= 0.2);
-        problem = transform_obj_scale(problem,a);
-        problem = transform_obj_shift(problem,b);
-    }
-    else if(instance > 50 && instance <= 100)
-    {
-        IOHprofiler_compute_xopt_double(xins,rseed,dimension);
-        for(i = 0; i < dimension; i++){
-            sigma[i] = (int)i;
-        }
-        for(i = 0; i < dimension; i++){
-            t = (int)(xins[i] * (double)dimension);
-            assert(t >= 0 && t < dimension);
-            temp = sigma[0];
-            sigma[0] = sigma[t];
-            sigma[t] = temp; 
-        }
+    else if(instance > 1 && instance <= 100){
         a = IOHprofiler_compute_fopt(function,instance + 100);
         a = fabs(a) / 1000 * 4.8 + 0.2;
         b = IOHprofiler_compute_fopt(function, instance);
-        problem = transform_vars_sigma(problem, sigma, 0);
         assert(a <= 5.0 && a >= 0.2);
+
         problem = transform_obj_scale(problem,a);
         problem = transform_obj_shift(problem,b);
     } else {
@@ -158,7 +134,5 @@ static IOHprofiler_problem_t *f_MIS_IOHprofiler_problem_allocate(const size_t fu
     IOHprofiler_problem_set_type(problem, "pseudo-Boolean");
 
     IOHprofiler_free_memory(z);
-    IOHprofiler_free_memory(sigma);
-    IOHprofiler_free_memory(xins);
     return problem;
 }
