20e66dd9c41cd6aeb7007030f6317f62df7f7b04
F. Ye <f.ye@liacs.leidenuiv.nl>
2019-03-29 18:53:59 +0100

    update R interface

--- code-experiments/build/R/Rcpp/interfaceR.cpp
+++ code-experiments/build/R/Rcpp/interfaceR.cpp
@@ -1,6 +1,10 @@
 #include <Rcpp.h>
 #include <string>
+
 #include "IOHprofiler.c"
+
+
+
 
 using namespace Rcpp;
 
@@ -41,8 +45,8 @@ IOHprofiler_problem_t* current_problem;
 
 //[[Rcpp::export]]
 void init_suite(String functions, String dimensions, String instances){
-	if(suite.current_suite != NULL){
-		IOHprofiler_suite_free(suite.current_suite);
+	if(current_suite != NULL){
+		IOHprofiler_suite_free(current_suite);
 		Rcout << "Current suite has been erased.\n";
 	}
 	String ins = " instances: ";
@@ -52,12 +56,8 @@ void init_suite(String functions, String dimensions, String instances){
 	func += functions;
 	dim += dimensions;
 	func += dim;
-	suite.current_suite = IOHprofiler_suite("PBO", ins.get_cstring() ,func.get_cstring());
-	suite.name = "PBO";
-	suite.instance = instances;
-	suite.dimension = dimensions;
-	suite.function = functions;
-	suite.state = 1;
+	current_suite = IOHprofiler_suite("PBO", ins.get_cstring() ,func.get_cstring());
+
 	Rcout << "Suite PBO has been initialized.\n";
 }
 
@@ -65,8 +65,8 @@ void init_suite(String functions, String dimensions, String instances){
 
 // //[[Rcpp::export]]
 // void default_suite(){
-// 	if(suite.current_suite != NULL){
-// 		IOHprofiler_suite_free(suite.current_suite);
+// 	if(current_suite != NULL){
+// 		IOHprofiler_suite_free(current_suite);
 // 		Rcout << "Current suite has been erased.\n";
 // 	}
 // 	String ins = " instances: ";
@@ -76,24 +76,28 @@ void init_suite(String functions, String dimensions, String instances){
 // 	func += "1-24";
 // 	dim += "100,500,1000,2000,3000";
 // 	func += dim;
-// 	suite.current_suite = IOHprofiler_suite("PBO", ins.get_cstring() ,func.get_cstring());
-// 	suite.name = "PBO";
-// 	suite.instance = "1-100";
-// 	suite.dimension = "100,500,1000,2000,3000";
-// 	suite.function = "1-24";
-// 	suite.state = 1;
+// 	current_suite = IOHprofiler_suite("PBO", ins.get_cstring() ,func.get_cstring());
+// 	name = "PBO";
+// 	instance = "1-100";
+// 	dimension = "100,500,1000,2000,3000";
+// 	function = "1-24";
+// 	state = 1;
 // 	Rcout << "Suite PBO has been initialized.\n";
 // }
 
 
-// Create an observer. All options are with default values. To config output path and format, apply following functions. 
+// Create an  All options are with default values. To config output path and format, apply following functions. 
 
 //[[Rcpp::export]]
-void init_observer(){
-	if(observer.current_observer != NULL){
-		IOHprofiler_observer_free(observer.current_observer);
-		Rcout << "Current observer has been erased.\n";
-	}
+void init_observer(String result_folder, String algorithm_name, String algorithm_info, 
+					String complete_triggers, int number_interval_triggers,
+					String base_evaluation_triggers, int number_target_triggers,
+					String parameters_name) {
+//	if(current_observer != NULL){
+//		IOHprofiler_observer_free(current_observer);
+//		Rcout << "Current observer has been erased.\n";
+//	}
+
 	char *observer_options = IOHprofiler_strdupf( "result_folder:  %s "
                                                 "algorithm_name: %s "
                                                 "algorithm_info: %s "
@@ -102,15 +106,13 @@ void init_observer(){
                                                 "base_evaluation_triggers: %s "
                                                 "number_target_triggers: %d "
                                                 "parameters_name: %s", 
-                                                observer.result_folder.get_cstring(),observer.algorithm_name.get_cstring(),
-                                                observer.algorithm_info.get_cstring(),observer.complete_triggers.get_cstring(),
-                                                observer.number_interval_triggers, observer.base_evaluation_triggers.get_cstring(),
-                                                observer.number_target_triggers,observer.parameters_name.get_cstring());
+                                                result_folder.get_cstring(),algorithm_name.get_cstring(),
+                                                algorithm_info.get_cstring(),complete_triggers.get_cstring(),
+                                                number_interval_triggers, base_evaluation_triggers.get_cstring(),
+                                                number_target_triggers,parameters_name.get_cstring());
  
-	observer.current_observer = IOHprofiler_observer("PBO", observer_options);
-	observer.name = "PBO";
-	observer.state = 1;
-	Rcout << "A new observer PBO has been created.\n";
+	current_observer = IOHprofiler_observer("PBO", observer_options);
+//	Rcout << "A new observer PBO has been created.\n";
 }
 
 
@@ -118,25 +120,25 @@ void init_observer(){
 
 // //[[Rcpp::export]]
 // void set_result_folder(String folder){
-// 	observer.result_folder = folder;
+// 	result_folder = folder;
 // 	Rcout << "Output path has been set as " << folder.get_cstring() << ".\n";
 // }
 
 // //[[Rcpp::export]]
 // void set_algorithm_name(String alg_name){
-// 	observer.algorithm_name = alg_name;
+// 	algorithm_name = alg_name;
 // 	Rcout << "Algorithm name has been set as " << alg_name.get_cstring() << ".\n";
 // }
 
 // //[[Rcpp::export]]
 // void set_algorithm_info(String alg_info){
-// 	observer.algorithm_info = alg_info;
+// 	algorithm_info = alg_info;
 // 	Rcout << "Algorithm info has been set as " << alg_info.get_cstring() << ".\n";
 // }
 
 // //[[Rcpp::export]]
 // void set_complete_trigger(String complete_trig){
-// 	observer.complete_triggers = complete_trig;
+// 	complete_triggers = complete_trig;
 // 	if(complete_trig == "True" || complete_trig == "true" || complete_trig == "TRUE"){
 // 		Rcout << ".cdat files that store evaluations for each iteration will be output.\n";
 // 	}else{
@@ -146,7 +148,7 @@ void init_observer(){
 
 // //[[Rcpp::export]]
 // void set_number_interval_triggers(int num_interval_trig){
-// 	observer.number_target_triggers = num_interval_trig;
+// 	number_target_triggers = num_interval_trig;
 // 	if(num_interval_trig == 0){
 // 		Rcout << ".idat files are closed.\n";
 // 	}else{
@@ -156,162 +158,237 @@ void init_observer(){
 
 // //[[Rcpp::export]]
 // void set_number_target_triggers(int num_target_targ){
-// 	observer.number_target_triggers = num_target_targ;
+// 	number_target_triggers = num_target_targ;
 // 	Rcout << "number_target_triggers has been set as " << num_target_targ << "\n.";
 // }
 
 // //[[Rcpp::export]]
 // void set_parameter_names(String para_names){
-// 	observer.parameters_name = para_names;
+// 	parameters_name = para_names;
 // 	Rcout << "Parameter names have been set as " << para_names.get_cstring() << ".\n Please confirm that there is no blank, and multiple names are separate by comma \',\'.\n";
 // }
 
-// Acquire a problem based on arguments, but the problem must be included in the suite.
+// Acquire a problem based on arguments, but the problem must be included in the 
 // @Furong: set each c procedure name with prefix 'c_'
+// //[[Rcpp::export]]
+// void c_set_function(int dimension, int problem_id, int instance_id){
+//	if(state == 0){
+//		Rcout << "Please create a suite at first.\n";
+//	}
+//	if(state == 0){
+//		Rcout << "Please create an observer at first.\n";
+//	}
+//	if(current_problem != NULL) {
+//		IOHprofiler_problem_free(current_problem);
+//	}
+//	current_problem = IOHprofiler_suite_get_problem_by_function_dimension_instance(current_suite,problem_id,dimension,instance_id);
+//	current_problem = IOHprofiler_problem_add_observer(current_problem,current_observer);
+//	if(current_problem == NULL){
+//		Rcout << "The problem is not found in \n";
+//	}else{
+//		Rcout << "Currently working on " << IOHprofiler_problem_get_name(current_problem) << ".\n";	
+//	}
+//}
+
+// Returns a list of problem ids.
+
 //[[Rcpp::export]]
-void c_set_function(int dimension, int function_id, int instance_id){
-	if(suite.state == 0){
-		Rcout << "Please create a suite at first.\n";
+IntegerVector get_problem_list() {
+	if(current_suite == NULL){
+		return NULL;
 	}
-	if(observer.state == 0){
-		Rcout << "Please create an observer at first.\n";
+	IntegerVector problem_list;
+	for (int i = 0; i < current_suite->number_of_functions; ++i)
+	{
+		if(current_suite->functions[i] != 0){
+			problem_list.push_back(current_suite->functions[i]);
+		}
 	}
-	suite.current_problem = IOHprofiler_suite_get_problem_by_function_dimension_instance(suite.current_suite,problem_id,dimension,instance_id);
-	if(suite.current_problem == NULL){
-		Rcout << "The problem is not found in suite.\n";
-	}else{
-		Rcout << "Currently working on " << IOHprofiler_problem_get_name(suite.current_problem) << ".\n";	
+	return problem_list;
+}
+
+// Returens a list of dimensions. 
+
+//[[Rcpp::export]]
+IntegerVector get_dimension_list() {
+	if(current_suite == NULL){
+		return NULL;
+	}
+	IntegerVector dimension_list;
+	for (int i = 0; i < current_suite->number_of_dimensions; ++i)
+	{
+		if(current_suite->dimensions[i] != 0){
+			dimension_list.push_back(current_suite->dimensions[i]);
+		}
+	}
+	return dimension_list;
+}
+
+
+
+//[[Rcpp::export]]
+List get_suite_info() {
+	if(current_suite == NULL) {
+		return NULL;
+	}
+	else{
+		return List::create(_["problems"] = get_problem_list(), 
+						_["dimensions"] =  get_dimension_list(),
+						_["instances"] = IntegerVector(current_suite->instances,current_suite->instances + current_suite->number_of_instances));
 	}
 }
+
+
+
+//[[Rcpp::export]]
+List get_problem_info() {
+	if(current_problem == NULL) {
+		return NULL;
+	}
+	else {
+		return List::create(_["problem"] = current_suite->functions[current_suite->current_function_idx] , 
+						_["dimension"] = current_problem->dimension,
+						_["instance"] = current_suite->instances[current_suite->current_instance_idx]);
+	}
+}
+
 
 // Acquire a problem in suite by order.
 
 //[[Rcpp::export]]
-void get_next_problem(){
-	// if(suite.state == 0){
+List get_next_problem() {
+	// if(state == 0){
 	// 	Rcout << "Please create a suite at first.\n";
 	// }
-	// if(observer.state == 0){
+	// if(state == 0){
 	// 	Rcout << "Please create an observer at first.\n";
 	// }
-	suite.current_problem = IOHprofiler_suite_get_next_problem(suite.current_suite, observer.current_observer);
-	if(suite.current_problem == NULL){
-		IOHprofiler_observer_free(observer.current_observer);
-  		IOHprofiler_suite_free(suite.current_suite);
-  		suite.state = 0;
-  		observer.state = 0;
-  		Rcout << "All problems have been tested, suite and observer are free. Thanks for using IOHExperimentor.\n";
+	if(current_suite != NULL && current_observer != NULL){
+		current_problem = IOHprofiler_suite_get_next_problem(current_suite, current_observer);
+		if(current_problem == NULL){
+			IOHprofiler_observer_free(current_observer);
+	  		IOHprofiler_suite_free(current_suite);
+	//  		Rcout << "All problems have been tested, suite and observer are free. Thanks for using IOHExperimentor.\n";
+		}
+		else{
+			return get_problem_info();
+	//		Rcout << "Currently working on " << IOHprofiler_problem_get_name(current_problem) << ".\n";
+		}
 	}
-	else{
-		Rcout << "Currently working on " << IOHprofiler_problem_get_name(suite.current_problem) << ".\n";
-	}
+	return NULL;
 }
 
 // To reset status of current problem. Applying this function to re-run current problem.
 
 //[[Rcpp::export]]
-void reset_problem(){
-	if(suite.current_problem == NULL){
+List reset_problem() {
+	if(current_problem == NULL){
 		Rcout << "There is no problem exists.\n";
+		return NULL;
 	}
 	else{
-		suite.current_problem = IOHprofiler_suite_reset_problem(suite.current_suite, observer.current_observer);
-		Rcout << IOHprofiler_problem_get_name(suite.current_problem) << "resets.\n";	
+		current_problem = IOHprofiler_suite_reset_problem(current_suite, current_observer);
+		return get_problem_info();
 	}
 }
 
-// // Returns a list of problem id of current suite.
-// //[[Rcpp::export]]
-// NumericVector get_problem_list(){
-// 	if(suite.current_suite == NULL){
-// 		Rcout << "There is no suite exists.\n";
-// 		return 1;
-// 	}
-// 	NumericVector problem_list;
-// 	for (int i = 0; i < suite.current_suite->number_of_functions; ++i)
-// 	{
-// 		if(suite.current_suite->functions[i] != 0){
-// 			problem_list.push_back(suite.current_suite->functions[i]);
-// 		}
-// 	}
-// 	return problem_list;
-// }
 
-// // Returens a list of dimensions of current suite.
-
-// //[[Rcpp::export]]
-// NumericVector get_dimension_list(){
-// 	if(suite.current_suite == NULL){
-// 		Rcout << "There is no suite exists.\n";
-// 		return 1;
-// 	}
-// 	NumericVector dimension_list;
-// 	for (int i = 0; i < suite.current_suite->number_of_dimensions; ++i)
-// 	{
-// 		if(suite.current_suite->dimensions[i] != 0){
-// 			dimension_list.push_back(suite.current_suite->dimensions[i]);
-// 		}
-// 	}
-// 	return dimension_list;
-// }
-
-// // Returns a list of instances of current suite.
+// 
 
 // //[[Rcpp::export]]
 // NumericVector get_instance_list(){
-// 	if(suite.current_suite == NULL){
+// 	if(current_suite == NULL){
 // 		Rcout << "There is no suite exists.\n";
 // 		return 1;
 // 	}
-// 	NumericVector instance_list(suite.current_suite->number_of_instances);
-// 	for(int i = 0; i < suite.current_suite->number_of_instances; ++i){
-// 		instance_list(i) = suite.current_suite->instances[i];
+// 	NumericVector instance_list(current_suite->number_of_instances);
+// 	for(int i = 0; i < current_suite->number_of_instances; ++i){
+// 		instance_list(i) = current_suite->instances[i];
 // 	} 
 // 	return instance_list;
 // }
 
 // Returns name string of current problem.
 
-//[[Rcpp::export]]
-String get_problem_info(){
-	if(suite.current_problem == NULL){
-		Rcout << "No problem exists.\n";
-		return 1;
-	}
-	return IOHprofiler_problem_get_name(suite.current_problem); 
-}
+
 
 // Return fitness of input variables.
 
 //[[Rcpp::export]]
-double c_eval(NumericVector x){
-	double *result = IOHprofiler_allocate_vector(1);
-	int n = x.size();
-	// if(n != suite.current_problem->number_of_variables){
-	// 	Rcout << "Error! Dimension of input vector is incorrect.\n";
-	// 	return -DBL_MAX;
-	// }
-
-	int *input = IOHprofiler_allocate_int_vector(n);
-	for(int i = 0; i < n; ++i){
-		input[i] = x(i);
+double c_eval(IntegerVector x) {
+	if(current_problem == NULL) {
+		return NULL;
 	}
-	IOHprofiler_evaluate_function(suite.current_problem, input, result);
+
+	double *result = IOHprofiler_allocate_vector(1);
+	
+	if(x.size() != current_problem->number_of_variables){
+		Rcout << "Error! Dimension of input vector is incorrect.\n";
+	 	return -DBL_MAX;
+	}
+
+	IOHprofiler_evaluate_function(current_problem, INTEGER(x), result);
 	return result[0];
 }
+
+// To test if the optimal has been found. Return 1 if it is found.
+
+//[[Rcpp::export]]
+int hit_optimal() {
+	if(current_problem == NULL) {
+		return -1;
+	}
+
+	return IOHprofiler_problem_final_target_hit(current_problem);
+}
+
+// Returns evaluation time has been done.
+
+//[[Rcpp::export]]
+int eval_times() {
+	if(current_problem == NULL) {
+		return -1;
+	}
+
+	return IOHprofiler_problem_get_evaluations(current_problem);
+}
+
+// Return statement of optimal variables.
+
+//[[Rcpp::export]]
+IntegerVector optimal_variables() {
+	if(current_problem == NULL) {
+		return NULL;
+	}
+
+	return IntegerVector(current_problem->best_parameter,current_problem->best_parameter+current_problem->number_of_variables);
+}
+
+// Return the optimal value.
+
+//[[Rcpp::export]]
+double optimal() {
+	if(current_problem == NULL) {
+		return -DBL_MAX;
+	}
+
+	return current_problem->best_value[0];
+}
+
 
 // Free memory.
 
 //[[Rcpp::export]]
-void IOHfree(){
-	if(suite.current_problem != NULL){
-		IOHprofiler_problem_free(suite.current_problem);
+void IOHfree() {
+	if(current_problem != NULL){
+		IOHprofiler_problem_free(current_problem);
 	}
-	if(observer.current_observer != NULL){
-		IOHprofiler_observer_free(observer.current_observer);
+	if(current_observer != NULL){
+		IOHprofiler_observer_free(current_observer);
 	}
-	if(suite.current_suite != NULL){
-  		IOHprofiler_suite_free(suite.current_suite);
+	if(current_suite != NULL){
+  		IOHprofiler_suite_free(current_suite);
   	}
   	Rcout << "IOHExperimentor free.\n";
 }
+
