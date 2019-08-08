067a37c0e215386b8bce8988dc5bdf186f122dda
F. Ye <f.ye@liacs.leidenuiv.nl>
2019-08-08 14:39:01 +0200

    Output of cpu time

--- src/Template/Experiments/IOHprofiler_experimenter.hpp
+++ src/Template/Experiments/IOHprofiler_experimenter.hpp
@@ -42,16 +42,34 @@ public:
   ~IOHprofiler_experimenter(){};
 
   void _run() {
+    std::clock_t c_start_overall = std::clock();
+
+    std::string info = "IOHprofiler_experiment\n";
+    info += "Suite: " + this->configSuite->IOHprofiler_suite_get_suite_name() + "\n";
+    info += "Problem: " + vectorToString(this->configSuite->IOHprofiler_suite_get_problem_id()) + "\n";
+    info += "Instance: " + vectorToString(this->configSuite->IOHprofiler_suite_get_instance_id()) + "\n";
+    info += "Dimension: " + vectorToString(this->configSuite->IOHprofiler_suite_get_dimension()) + "\n";
+    print_info(info);
+
     this->config_csv_logger->target_suite(this->configSuite->IOHprofiler_suite_get_suite_name());
 
     /// Problems are tested one by one until 'get_next_problem' returns NULL.
     while (current_problem = configSuite->get_next_problem()) {
+      std::clock_t c_start = std::clock();
+      info = "f";
+      info += std::to_string(current_problem->IOHprofiler_get_problem_id());
+      info += "_d" + std::to_string(current_problem->IOHprofiler_get_problem_id());
+      info += "_i" + std::to_string(current_problem->IOHprofiler_get_instance_id());
+      print_info(info);
 
       this->config_csv_logger->target_problem(current_problem->IOHprofiler_get_problem_id(), 
                                               current_problem->IOHprofiler_get_number_of_variables(), 
                                               current_problem->IOHprofiler_get_instance_id());
 
       algorithm(current_problem,this->config_csv_logger);
+      
+      print_info(".");
+
       int count = 1;
       while(independent_runs > count) {
         current_problem = configSuite->get_current_problem();
@@ -60,15 +78,39 @@ public:
                                               current_problem->IOHprofiler_get_instance_id());
         algorithm(current_problem,this->config_csv_logger);
         ++count;
+
+        print_info(".");
       }
+
+      std::clock_t c_end = std::clock();
+      print_info("CPU Time" + std::to_string(1000.0 * (c_end-c_start) / CLOCKS_PER_SEC) + "ms\n");
     }
+
+    std::clock_t c_end_overall = std::clock();
+    print_info("Total CPU Time" + std::to_string(1000.0 * (c_end_overall-c_start_overall) / CLOCKS_PER_SEC) + "ms\n");
   };
 
   void _set_independent_runs(int n) {
     this->independent_runs = n;
   }
 
+  void print_info(std::string info) {
+    std::cout << info << std::flush;
+  }
   
+  std::string vectorToString(std::vector<int> v) {
+    
+    std::string s = "";
+    if (v.size() == 0) {
+      return s;
+    }
+    s = std::to_string(v[0]);
+    for (int i = 1; i != v.size(); ++i) {
+      s += " " + std::to_string(v[i]);
+    }
+
+    return s;
+  }
 
 private:
   IOHprofiler_configuration conf;
