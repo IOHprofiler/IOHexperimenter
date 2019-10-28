c7ff24ca9be6d9d179e24a5817aebcb3261af51a
F. Ye <f.ye@liacs.leidenuiv.nl>
2019-10-28 13:40:01 +0100

    Rename definition of PI by IOH_PI

--- src/Template/IOHprofiler_random.hpp
+++ src/Template/IOHprofiler_random.hpp
@@ -10,7 +10,7 @@
 
 #include "IOHprofiler_common.h"
 
-#define PI 3.14159265359
+#define IOH_PI 3.14159265359
 #define DEFAULT_SEED 1000
 /* OMS: mutators vs. accessors should be checked throughout; see example in l.137 where const is needed. */
 class IOHprofiler_random {
@@ -119,7 +119,7 @@ public:
      IOHprofiler_uniform_rand(2 * N, seed,uniform_rand_vec);
 
     for (int i = 0; i < N; i++) {
-      rand_vec.push_back(sqrt(-2 * log(uniform_rand_vec[i])) * cos(2 * PI * uniform_rand_vec[N + i]));
+      rand_vec.push_back(sqrt(-2 * log(uniform_rand_vec[i])) * cos(2 * IOH_PI * uniform_rand_vec[N + i]));
       if (rand_vec[i] == 0.) {
         rand_vec[i] = 1e-99;
       }
