fcb24b4c8f65f659623ecf0efaf58c1b97751cf7
F. Ye <f.ye@liacs.leidenuiv.nl>
2020-03-04 15:41:09 +0100

    update dir of include files for using CMake

--- build/Cpp/IOHprofiler_run_problem.cpp
+++ build/Cpp/IOHprofiler_run_problem.cpp
@@ -1,7 +1,7 @@
 
-#include "IOH/f_one_max.hpp"
-#include "IOH/f_w_model_one_max.hpp"
-#include "IOH/IOHprofiler_csv_logger.h"
+#include <f_one_max.hpp>
+#include <f_w_model_one_max.hpp>
+#include <IOHprofiler_csv_logger.h>
 
 
 std::vector<int> Initialization(int dimension) {
--- build/Cpp/IOHprofiler_run_suite.cpp
+++ build/Cpp/IOHprofiler_run_suite.cpp
@@ -1,5 +1,5 @@
-#include "IOH/IOHprofiler_PBO_suite.hpp"
-#include "IOH/IOHprofiler_csv_logger.h"
+#include <IOHprofiler_PBO_suite.hpp>
+#include <IOHprofiler_csv_logger.h>
 
 std::vector<int> Initialization(int dimension) {
   std::vector<int> x;
