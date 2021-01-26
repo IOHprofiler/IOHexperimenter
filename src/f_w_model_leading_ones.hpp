2aabc892ba5d240cfe2320fb89eb7d33b8068238
Furong Ye <furong1993@gmail.com>
2021-01-26 10:41:09 +0100

    bug fix : the optimal value setting for w-model extensions

--- src/f_w_model_leading_ones.hpp
+++ src/f_w_model_leading_ones.hpp
@@ -18,7 +18,6 @@ public:
     IOHprofiler_set_number_of_objectives(1);
     IOHprofiler_set_lowerbound(0);
     IOHprofiler_set_upperbound(1);
-    IOHprofiler_set_best_variables(1);
     IOHprofiler_set_number_of_variables(dimension);
   }
 
