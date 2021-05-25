41b474187b78ebcb70ed21288e5d32eefe6be409
Johann Dreo <johann@dreo.fr>
2021-05-25 12:26:13 +0200

    add missing triggers
    - AtInterval, AtTimePoints, PerTimeRange.
    - Use a simpler naming scheme for helpers: each, at and during.

--- include/ioh/logger/triggers.hpp
+++ include/ioh/logger/triggers.hpp
@@ -20,6 +20,20 @@ namespace ioh {
             // Fire log events only when the best transformed objective function value found so far has improved.
             ioh::logger::Store store_all_bests({ioh::trigger::on_improvement},{ioh::watch::transformed_y_best});
 
+            // Do log regularly along the run, or if there is an improvement.
+            ioh::logger::Store store_regularly({ // Several triggers defaults to trigger::any. Thus, fires if:
+                    ioh::trigger::each(10),      // each 10 calls, OR...
+                    ioh::trigger::on_improvement // on improvement.
+                }, {ioh::watch::transformed_y_best});
+
+            // Do log at the 10 first and 10 last calls of a 100-evaluations run.
+            ioh::logger::Store store_some({ioh::trigger::during({{0,10},{90,100}})}, {ioh::watch::transformed_y_best});
+
+            // Do log if there is improvement at the beginning or the end, but not in between.
+            ioh::logger::Store complex_store({ioh::trigger::all({ // Fires only if all sub-triggers do fire, that is if:
+                    ioh::trigger::on_improvement,                 // there is improvement, AND...
+                    ioh::trigger::during({{0,10},{90,100}})       // at the beginning or end.
+                }, {ioh::watch::transformed_y_best});
          * @endcode
          * 
          * @ingroup Loggers
@@ -164,7 +178,7 @@ namespace ioh {
                 return true;
             }
         };
-        /** Log at every call of the objective function.
+        /** Do log at every call of the objective function.
          *
          * @ingroup Triggers
          */
@@ -218,13 +232,144 @@ namespace ioh {
                 _has_type = false;
             }
         };
-        /** Log only if the transformed best objective function value found so far has strictly improved.
+        /** Do log only if the transformed best objective function value found so far has strictly improved.
          * 
          * @ingroup Triggers
          */
-         OnImprovement on_improvement;
+        OnImprovement on_improvement;
 
+        /** A trigger that fire at a regular interval.
+         * 
+         * @ingroup Triggering
+         */
+        class AtInterval : public logger::Trigger {
+        protected:
+            const size_t _interval;
+            const size_t _starting_at;
 
-        // TODO HERE: AtInterval AtTimePoints PerTimeRange
+        public:
+            AtInterval(const size_t interval, const size_t starting_at = 0)
+            : _interval(interval)
+            , _starting_at(starting_at)
+            { }
+
+            bool operator()(const log::Info& log_info, const problem::MetaData& pb_info) override
+            {
+                if((log_info.evaluations-_starting_at) % _interval == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        /** Do log every given number of function evaluations.
+         * 
+         * Starts counting at zero by default.
+         * 
+         * @ingroup Triggers
+         */
+        AtInterval& each(const size_t interval, const size_t starting_at = 0)
+        {
+           auto t = std::make_shared<AtInterval>(interval, starting_at);
+           return *t;
+        }
+
+        /** A trigger that fire only at specific number of evaluations.
+         *
+         * @ingroup Triggering
+         */
+        class AtTimePoints : public logger::Trigger {
+        protected:
+            const std::set<size_t> _time_points;
+
+            bool matches(const size_t evals)
+            {
+                return std::find( std::begin(_time_points), std::end(_time_points), evals)
+                       != std::end(_time_points);
+            }
+
+        public:
+            AtTimePoints(const std::set<size_t> time_points)
+            : _time_points(time_points)
+            {
+                assert(not time_points.empty());
+            }
+
+            bool operator()(const log::Info& log_info, const problem::MetaData& pb_info) override
+            {
+                if(matches(log_info.evaluations)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        /** Do log at the given number of evaluations.
+         * 
+         * @ingroup Triggers
+         */
+        AtTimePoints& at(const std::set<size_t> time_points)
+        {
+            auto t = std::make_shared<AtTimePoints>(time_points);
+            return *t;
+        }
+
+        /** A trigger that fire only when the number of evaluations is within certain range(s).
+         *
+         * @note Ranges are closed intervals: with `[start,end]`, the trigger will fire for start and end.
+         *       Ranges may overlap with each other.
+         * 
+         * @ingroup Triggering
+         */
+        class PerTimeRange : logger::Trigger {
+        protected:
+            const std::set<std::pair<size_t,size_t>> _time_ranges;
+
+            bool matches(const size_t evals)
+            {
+                for(const auto& r : _time_ranges) {
+                    assert(r.first <= r.second);
+                    if(r.first <= evals and evals <= r.second) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        public:
+            PerTimeRange( std::set<std::pair<size_t,size_t>> time_ranges )
+            : _time_ranges(time_ranges)
+            {
+                assert(not _time_ranges.empty());
+#ifndef NDEBUG
+                for(const auto& r : _time_ranges) {
+                    assert(r.first <= r.second); // less or equal, because it can be a single time point.
+                }
+#endif
+            }
+
+            bool operator()(const log::Info& log_info, const problem::MetaData& pb_info) override
+            {
+               if(matches(log_info.evaluations)) {
+                   return true;
+               } else {
+                   return false;
+               }
+            }
+
+        };
+        /** Do log if the number of evaluations falls in at least one of the given ranges.
+         * 
+         * @note Ranges are closed intervals `[start,end]`, the trigger will fire for start and end.
+         *       Ranges may overlap with each other.
+         * 
+         * @ingroup Triggers
+         */
+        PerTimeRange& during(const std::set<std::pair<size_t,size_t>> time_ranges)
+        {
+            auto t = std::make_shared<PerTimeRange>(time_ranges);
+            return *t;
+        }
+
     } // trigger
 } // ioh
