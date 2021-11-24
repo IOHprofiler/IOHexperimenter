eae8fdb5c8e240781d39dd1a71d286918d0d3c86
jacobdenobel <jacobdenobel@gmail.com>
2021-11-24 16:42:53 +0100

    update examples with new wrap problem

--- example/main.cpp
+++ example/main.cpp
@@ -33,44 +33,12 @@ void show_registered_objects()
     }
 }
 
-double fn(const std::vector<double> &x) { 
-    return std::accumulate(x.begin(), x.end(), 0.0); 
-}
-
-ioh::problem::Solution<double> calculate_objective(const int iid, const int dim)
-{
-    return {std::vector<double>(dim, iid), static_cast<double>(iid * dim)};
-}
-
-std::vector<double> tx(std::vector<double> x, const int iid){
-    x.at(0) = static_cast<double>(iid);
-    return x;
-}
-
-double ty(const double y, const int iid){
-    return y * iid;
-}
-
 
 int main()
 {
-    // show_registered_objects();
-    // suite_example();
-    // logger_example();
-    // problem_example();
-    // experiment_example();
-    using namespace ioh::common;
-    using namespace ioh::problem;
-
-    auto &factory = ProblemFactoryType<Problem<double>>::instance();
-    wrap_function<double>(fn, "fn", OptimizationType::Minimization, -5, 5, 
-                        tx, ty, calculate_objective);
-
-    std::vector<double> x0 = {1, 0, 2.5};
-
-    for (auto inst: {1, 2, 3}){
-        auto problem = factory.create("fn", inst, 3);
-        std::cout << *problem << std::endl;
-        std::cout << (*problem)(x0) << std::endl;
-    }
+    show_registered_objects();
+    suite_example();
+    logger_example();
+    problem_example();
+    experiment_example();
 }
