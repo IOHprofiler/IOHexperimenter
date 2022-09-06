7947cf5c10b6e30062ee283ef35709be85741697
jacobdenobel <jacobdenobel@gmail.com>
2022-09-06 20:16:21 +0200

    change to float eq in tests

--- tests/cpp/problem/test_constraints.cpp
+++ tests/cpp/problem/test_constraints.cpp
@@ -11,16 +11,16 @@ TEST_F(BaseTest, BoxBBOBConstraint)
 
     Sphere p(1, 1);
 
-    EXPECT_DOUBLE_EQ(p({10.}), 174.48792f);
-    EXPECT_DOUBLE_EQ(p.constraints().violation(), 0);
+    EXPECT_FLOAT_EQ((float)p({10.}), 174.48792f);
+    EXPECT_FLOAT_EQ((float)p.constraints().violation(), 0.f);
 
     p.enforce_bounds(std::numeric_limits<double>::infinity());
  
 	EXPECT_EQ(p({10}), std::numeric_limits<double>::infinity());
-    EXPECT_DOUBLE_EQ(p.constraints().violation(), 25);
+    EXPECT_FLOAT_EQ((float)p.constraints().violation(), 25.f);
 	
 	EXPECT_EQ(p({-10}), std::numeric_limits<double>::infinity());
-    EXPECT_DOUBLE_EQ(p.constraints().violation(), 25);
+    EXPECT_FLOAT_EQ((float)p.constraints().violation(), 25.f);
 }
 
 TEST_F(BaseTest, BoundsAsConstraints) {
@@ -32,7 +32,7 @@ TEST_F(BaseTest, BoundsAsConstraints) {
     EXPECT_EQ(p({10.}), std::numeric_limits<double>::infinity());
     
     p.enforce_bounds(1.0);
-    EXPECT_DOUBLE_EQ(p({10.}), p.state().y_unconstrained + 25);
+    EXPECT_FLOAT_EQ((float)p({10.}), (float)p.state().y_unconstrained + 25);
 
     EXPECT_EQ(p.constraints().n(), 1);
 }
@@ -43,26 +43,26 @@ TEST_F(BaseTest, BoxPBOConstraint)
     using namespace ioh::problem::pbo;
 
     LeadingOnes p(1, 1);
-    EXPECT_DOUBLE_EQ(p({1}), 1);
-    EXPECT_DOUBLE_EQ(p({2}), 0);
-    EXPECT_DOUBLE_EQ(p.constraints().violation(), 0);
+    EXPECT_FLOAT_EQ((float)p({1}), 1);
+    EXPECT_FLOAT_EQ((float)p({2}), 0);
+    EXPECT_FLOAT_EQ((float)p.constraints().violation(), 0.f);
 
     p.enforce_bounds(-std::numeric_limits<double>::infinity());
 
     EXPECT_EQ(p({2}), -std::numeric_limits<double>::infinity());
-    EXPECT_DOUBLE_EQ(p.constraints().violation(), 1);
+    EXPECT_FLOAT_EQ((float)p.constraints().violation(), 1.f);
 
     EXPECT_EQ(p({-2}), -std::numeric_limits<double>::infinity());
-    EXPECT_DOUBLE_EQ(p.constraints().violation(), 4);
+    EXPECT_FLOAT_EQ((float)p.constraints().violation(), 4.f);
 }
 
 //! constraint on y: y < 35
-double y_cons(const std::vector<double> &x, const double y) { 
+double y_cons(const std::vector<double> &, const double y) { 
     return std::max(0.0, y - 35.); 
 }
 
 //! constraint on x: x[i-1] < x[i]
-double x_cons(const std::vector<double> &x, const double y)
+double x_cons(const std::vector<double> &x, const double)
 {
     double violation = 0.0;
     for (size_t i = 1; i < x.size(); i++)
@@ -84,19 +84,19 @@ TEST_F(BaseTest, FunctionalConstraint) {
     
     p.add_constraint(c1);
 
-    EXPECT_DOUBLE_EQ(y0 + y_cons(x0, y0), p(x0));
+    EXPECT_FLOAT_EQ((float)y0 + y_cons(x0, y0), (float)p(x0));
 
     p.remove_constraint(c1);
     
-    EXPECT_DOUBLE_EQ(y0, p(x0));
+    EXPECT_FLOAT_EQ((float)y0, (float)p(x0));
 
     p.add_constraint(c2);
 
-    EXPECT_DOUBLE_EQ(y0 + 10 * x_cons(x0, y0), p(x0));
+    EXPECT_FLOAT_EQ((float)y0 + 10 * x_cons(x0, y0), (float)p(x0));
 
     p.add_constraint(c1);
 
-    EXPECT_DOUBLE_EQ(y0 + (10 * x_cons(x0, y0)) + y_cons(x0, y0), p(x0));
+    EXPECT_FLOAT_EQ((float)y0 + (10 * x_cons(x0, y0)) + y_cons(x0, y0), (float)p(x0));
 }
 
 
@@ -108,7 +108,7 @@ TEST_F(BaseTest, GraphConstraint) {
 
     const auto x1 = std::vector<int>(problem->meta_data().n_variables, 1);
     const auto y1 = (*problem)(x1);
-    EXPECT_DOUBLE_EQ(y1, problem->constraints().violation());
+    EXPECT_FLOAT_EQ((float)y1, (float)problem->constraints().violation());
 
     auto x0 = std::vector<int>(problem->meta_data().n_variables, 0);
     x0[0] = 1;
@@ -116,8 +116,8 @@ TEST_F(BaseTest, GraphConstraint) {
     
     const auto y0 = (*problem)(x0);
 
-    EXPECT_DOUBLE_EQ(y0, 81.);
-    EXPECT_DOUBLE_EQ(problem->constraints()[0]->violation(), 1.);
-    EXPECT_DOUBLE_EQ(problem->constraints()[1]->violation(), 0.);
-    EXPECT_DOUBLE_EQ(problem->constraints().violation(), 1.);
+    EXPECT_FLOAT_EQ((float)y0, 81.f);
+    EXPECT_FLOAT_EQ((float)problem->constraints()[0]->violation(), 1.f);
+    EXPECT_FLOAT_EQ((float)problem->constraints()[1]->violation(), 0.f);
+    EXPECT_FLOAT_EQ((float)problem->constraints().violation(), 1.f);
 }
