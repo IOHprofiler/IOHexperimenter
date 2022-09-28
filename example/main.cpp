b96ac8d789555d7dc66551fc5870400e880f71c6
jacobdenobel <jacobdenobel@gmail.com>
2022-09-28 14:38:03 +0200

    fix examples

--- example/main.cpp
+++ example/main.cpp
@@ -37,25 +37,10 @@ void show_registered_objects()
 
 int main()
 {
-    // show_registered_objects();
-    // problem_example();
-    // suite_example();
-    // experiment_example();
-    // logger_example();
-
-    // fmt::print("{}\n", std::numeric_limits<double>::infinity());
-    
-    // std::cout << fmt::format("{:.10f}", std::numeric_limits<double>::infinity()) << std::endl;
-    // double d = std::numeric_limits<double>::infinity();
-
-    // std::cout << fmt::format("{:.10f}", d) << std::endl;
-
-    // d = -std::numeric_limits<double>::infinity();
-    // std::cout << fmt::format("{:.10f}", d) << std::endl;
-
-    auto p = ioh::problem::bbob::Sphere(1, 2);
-    auto li = p.log_info();
-    li.raw_y = p.meta_data().initial_objective_value;
-    std::cout << ioh::watch::raw_y.call_to_string(li) << std::endl;
+    show_registered_objects();
+    problem_example();
+    suite_example();
+    experiment_example();
+    logger_example();
 }
 
