33ebd9bb284c8c37103c83e2b77225301b015051
jacobdenobel <jacobdenobel@gmail.com>
2022-10-10 14:32:25 +0200

    ci

--- tests/python/test_package.py
+++ tests/python/test_package.py
@@ -13,11 +13,14 @@ class TestPackage(unittest.TestCase):
     @unittest.skipUnless(sys.platform != "win32", "pip location on windows")
     def test_can_install_source_dist(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
-            subprocess.check_call([sys.executable, 
+            result = subprocess.run([sys.executable, 
                 os.path.join(DIR, "setup.py"), "sdist", f"--dist-dir={tmpdirname}"],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE
             )
+            
+            print(result.stderr)
+            self.assertEqual(result.returncode, 0)
             
             source_dist, *_ = glob.glob(f"{tmpdirname}/*")
             self.assertTrue(os.path.isfile(source_dist))
