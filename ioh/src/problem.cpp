0202700a8f521ddaa4f260fd1a99181e5d14baa4
DVermetten <d.vermetten@gmail.com>
2023-05-12 14:37:09 +0200

    Update problem documentation python

--- ioh/src/problem.cpp
+++ ioh/src/problem.cpp
@@ -158,7 +158,7 @@ void define_functionalconstraint(py::module &m, const std::string &name)
                  }),
              py::arg("fn"), py::arg("weight") = 1.0, py::arg("exponent") = 1.0,
              py::arg("enforced") = constraint::Enforced::SOFT,
-             py::arg("name") = ""
+             py::arg("name") = "",
                                R"pbdoc(
                 General Constraint, defined by a function
 
@@ -171,7 +171,7 @@ void define_functionalconstraint(py::module &m, const std::string &name)
                     exponent: float
                         The exponent given to the violation when penalizing the objective function
                     enforced: ConstraintEnforcement
-                        whether the constraint should be enforced
+                        how the constraint should be enforced
                     name: str
                         name of the constraint                    
             )pbdoc")
@@ -407,7 +407,15 @@ void define_base_class(py::module &m, const std::string &name)
         .def("add_constraint", &ProblemType::add_constraint, "add a constraint")
         .def("remove_constraint", &ProblemType::remove_constraint, "remove a constraint")
         .def("enforce_bounds", &ProblemType::enforce_bounds, py::arg("weight") = 1.,
-             py::arg("how") = constraint::Enforced::SOFT, py::arg("exponent") = 1.)
+             py::arg("how") = constraint::Enforced::SOFT, py::arg("exponent") = 1., 
+            R"pbdoc(
+                Enforced the bounds (box-constraints) as constraint
+                Parameters
+                ----------
+                    weight: The weight for computing the penalty (can be infinity to have strict box-constraints)
+                    how: The enforcement strategy, should be one of the 'ioh.ConstraintEnforcement' options
+                    exponent: The exponent for scaling the contraint
+                )pbdoc")
         .def("__repr__", [=](const ProblemType &p) {
             using namespace ioh::common;
             const auto meta_data = p.meta_data();
@@ -497,12 +505,17 @@ void define_helper_classes(py::module &m)
         .export_values();
 
     py::enum_<ioh::problem::constraint::Enforced>(m, "ConstraintEnforcement",
-                                                  "Enum defining constraint handling strategies")
-        .value("NOT", ioh::problem::constraint::Enforced::NOT)
-        .value("HIDDEN", ioh::problem::constraint::Enforced::HIDDEN)
-        .value("SOFT", ioh::problem::constraint::Enforced::SOFT)
-        .value("HARD", ioh::problem::constraint::Enforced::HARD)
-        .value("OVERRIDE", ioh::problem::constraint::Enforced::OVERRIDE)
+                                                  "Enum defining constraint handling strategies.")
+        .value("NOT", ioh::problem::constraint::Enforced::NOT, 
+                "Do not calculate the constraint at all")
+        .value("HIDDEN", ioh::problem::constraint::Enforced::HIDDEN, 
+                "Do not enforce, but still calculate to enable logging.")
+        .value("SOFT", ioh::problem::constraint::Enforced::SOFT,
+               "Penalize (y + p), but aggregate all the constraint penalties into a sum ")
+        .value("HARD", ioh::problem::constraint::Enforced::HARD,
+               "Penalize (p only), and if violation return only the penalty for this constraint in contraintset")
+        .value("OVERRIDE", ioh::problem::constraint::Enforced::OVERRIDE,
+               "Penalize (p only), and if violation return the custom penalization function this constraint in contraintset")
         .export_values();
 
     define_solution<double, double>(m, "RealSolution");
@@ -1018,7 +1031,20 @@ void define_submodular_problems(py::module &m)
 
 
     py::class_<GraphProblem, IntegerSingleObjective, std::shared_ptr<GraphProblem>>(m, "GraphProblem",
-                                                                                    "Graph type problem")
+                                                                                    "Graph type problem", 
+        R"pbdoc(
+            Graph-based problems (submodular problems)
+             
+            These submodular problems are all based on an underlying graph structure. 
+
+            Reference
+            ---------
+            [Neumann23] Neumann, Frank, Aneta Neumann, Chao Qian, Viet Anh Do, Jacob de Nobel, Diederick Vermetten, 
+            Saba Sadeghi Ahouei, Furong Ye, Hao Wang, and Thomas Bäck. 
+            "Benchmarking Algorithms for Submodular Optimization Problems Using IOHProfiler." 
+            arXiv preprint arXiv:2302.01464 (2023).
+
+        )pbdoc")
         .def_static(
             "create",
             [](const std::string &name, int iid, int dim) {
@@ -1061,24 +1087,117 @@ void define_submodular_problems(py::module &m)
             "All registered problems");
 
 
-    py::class_<MaxCut, GraphProblem, std::shared_ptr<MaxCut>>(m, "MaxCut", py::is_final(), "MaxCut function")
+    py::class_<MaxCut, GraphProblem, std::shared_ptr<MaxCut>>(m, "MaxCut", py::is_final(), R"pbdoc(
+            Max-Cut problems
+             
+            The maximum cut problem is a classical NP-hard problem and can be defined as follows. Given an undirected
+            weighted graph G = (V, E, w) with weights w: E → R≥0 on the edges, the goal is to select a set V1 ⊆ V such that
+            the sum of the weight of edges between V1 and V2 = V \ V1 is maximal.
+
+            For a given search point x ∈ {0, 1}^n where n = |V|, we have V1(x) = {vi | xi = 1} and V2(x) = {vi | xi = 0}. 
+            Let C(x) = {e ∈ E | e ∩ V1(x) 6= ∅ ∧ e ∩ V2(x) 6= ∅} be the cut of a given search point x. The goal is to maximize
+            f'(x) = Sum_{e∈C(x)} w(e).
+
+            Reference
+            ---------
+            [Neumann23] Neumann, Frank, Aneta Neumann, Chao Qian, Viet Anh Do, Jacob de Nobel, Diederick Vermetten, 
+            Saba Sadeghi Ahouei, Furong Ye, Hao Wang, and Thomas Bäck. 
+            "Benchmarking Algorithms for Submodular Optimization Problems Using IOHProfiler." 
+            arXiv preprint arXiv:2302.01464 (2023).
+
+        )pbdoc" 
+        )
         .def_static("load_instances", &GraphProblemType<MaxCut>::load_graph_instances<int, int>,
                     py::arg("path") = std::nullopt);
 
     // Don't allow these object to be created in from constructors in python
     py::class_<MaxCoverage, GraphProblem, std::shared_ptr<MaxCoverage>>(m, "MaxCoverage", py::is_final(),
-                                                                        "MaxCoverage function")
+                                                                        R"pbdoc(
+            Max-Coverage problems
+             
+            The maximum coverage problem is a classical optimization problem on graphs. Given an undirected weighted
+            graph G = (V, E, c) with costs c : V → R≥0 on the vertices. We denote by N(V') = 
+            {vi | ∃e ∈ E : e ∩ V' != ∧ e ∩ vi != ∅} the set of all nodes of V' and their neighbors in G.
+            For a given search point x ∈ {0, 1}^n where n = |V |, we have V'(x) = {vi | xi = 1} and 
+            c(x) = Sum_{v∈V 0(x)} c(v).
+
+            In the deterministic setting, the goal is to maximize f(x) = |N(V'(x))|
+            under the constraint that c(x) ≤ B holds.
+
+            Reference
+            ---------
+            [Neumann23] Neumann, Frank, Aneta Neumann, Chao Qian, Viet Anh Do, Jacob de Nobel, Diederick Vermetten, 
+            Saba Sadeghi Ahouei, Furong Ye, Hao Wang, and Thomas Bäck. 
+            "Benchmarking Algorithms for Submodular Optimization Problems Using IOHProfiler." 
+            arXiv preprint arXiv:2302.01464 (2023).
+
+        )pbdoc")
         .def_static("load_instances", &GraphProblemType<MaxCoverage>::load_graph_instances<int, int>,
                     py::arg("path") = std::nullopt);
 
 
     py::class_<MaxInfluence, GraphProblem, std::shared_ptr<MaxInfluence>>(m, "MaxInfluence", py::is_final(),
-                                                                          "MaxInfluence function")
+                                                                          R"pbdoc(
+            Max-Influence problems
+             
+            The maximum influence problem in social networks is an important submodular optimization problem that has been
+            widely studied in the literature from various perspectives. Let a directed graph G(V, E) represent a social network, 
+            where each node is a user and each edge (u, v) ∈ E has a probability pu,v representing the strength of influence from user u to v.
+
+            A fundamental propagation model is independence cascade. Starting from a seed set X, it uses a set At to record the
+            nodes activated at time t, and at time t + 1, each inactive neighbor v of u ∈ At becomes active with probability pu,v.
+            This process is repeated until no nodes get activated at some time. The set of nodes activated by propagating from X
+            is denoted as IC(X), which is a random variable.
+
+            The goal is to maximize the expected value of IC(X). Note that the computation of the expected value is done by
+            running a simulation of the influence process several times and averaging its results. In this sense, the computation of
+            the objective function value is stochastic.
+
+            For a given search point x ∈ {0, 1}^n where n = |V|, we have V'(x) = {vi | xi = 1} and c(x) = SUM_{v∈V 0(x)} c(v).
+
+
+            Reference
+            ---------
+            [Neumann23] Neumann, Frank, Aneta Neumann, Chao Qian, Viet Anh Do, Jacob de Nobel, Diederick Vermetten, 
+            Saba Sadeghi Ahouei, Furong Ye, Hao Wang, and Thomas Bäck. 
+            "Benchmarking Algorithms for Submodular Optimization Problems Using IOHProfiler." 
+            arXiv preprint arXiv:2302.01464 (2023).
+        )pbdoc")
         .def_static("load_instances", &GraphProblemType<MaxInfluence>::load_graph_instances<int, int>,
                     py::arg("path") = std::nullopt);
 
     py::class_<PackWhileTravel, GraphProblem, std::shared_ptr<PackWhileTravel>>(m, "PackWhileTravel", py::is_final(),
-                                                                                "PackWhileTravel function")
+                                                                                R"pbdoc(
+            Packing while traveling problems
+             
+            The packing while traveling (PWT) problem is a non-monotone submodular optimization problem which is
+            obtained from the traveling thief problem (TTP) when the route is fixed.
+
+            The input is given as n + 1 cities with distances di, 1 ≤ i ≤ n, from city i to city i + 1. Each city i, 1 ≤ i ≤ n,
+            contains a set of items Mi ⊆ M, |Mi| = mi. Each item eij ∈ Mi, 1 ≤ j ≤ mi, has a positive integer profit pij and
+            weight wij . A fixed route N = (1, 2, ..., n + 1) is traveled by a vehicle with velocity v ∈ [vmin, vmax]. We denote by
+            xij ∈ {0, 1} the variable indicating whether or not item eij is chosen in a solution 
+            x = (x11, x12, ..., x1m1, x21, ..., xnmn) ∈ {0, 1}^m, where m = SUM_{n}^{i=1} mi. 
+            The total benefit of selecting a subset of items selected by x is given as PWT(x) = P(x) − R · T(x),
+            where P(x) is the total profit of the selected items and T(x) is the total travel time for the vehicle carrying the selected
+            items. Formally, we have P(x) = Sum_{i=1}^n Sum_{j=1}^{mi} pij xij and
+            T(x) = Sum_{i=1}^n di / (vmax − ν Sum_{k=1}^i Sum_{j=1}^{mk} wkj xkj
+            Here, ν = (vmax−vmin) / B is a constant defined by the input parameters, where B is the capacity of the vehicle. The
+            problem is already NP-hard without any additional constraints, but often considered with a typical knapsack
+            constraint given as c(x) = Sum_{i=1}^n Sum_{j=1}^{mi} wij xij ≤ B.
+            As fitness functions, we use g(x) = (f'(x), c(x)) with
+            f'(x) = PWT(x) if c(x) ≤ B
+            and f'(x) = B − c(x) − R · T(vmin) if c(x) > B
+            where T(vmin) = (1 / vmin) Sum_i=1}^n di is the travel time at speed vmin.
+
+
+            Reference
+            ---------
+            [Neumann23] Neumann, Frank, Aneta Neumann, Chao Qian, Viet Anh Do, Jacob de Nobel, Diederick Vermetten, 
+            Saba Sadeghi Ahouei, Furong Ye, Hao Wang, and Thomas Bäck. 
+            "Benchmarking Algorithms for Submodular Optimization Problems Using IOHProfiler." 
+            arXiv preprint arXiv:2302.01464 (2023).
+        )pbdoc")
         .def_static("load_instances", &GraphProblemType<PackWhileTravel>::load_graph_instances<int, int>,
                     py::arg("path") = std::nullopt);
 }
@@ -1117,7 +1236,31 @@ T star_discrepancy_init(const int instance, const int n_variables, const int n_s
 template <typename T, typename P>
 void define_star_discrepancy_problem(py::module &m, const std::string &name)
 {
-    py::class_<T, P, std::shared_ptr<T>>(m, name.c_str())
+    py::class_<T, P, std::shared_ptr<T>>(m, name.c_str(), R"pbdoc(
+            Star-discrepancy problems
+             
+            The 𝐿_infinity star discrepancy is a measure for the regularity of a finite set
+            of points taken from [0, 1)^d. Calculating this measure is challenging, and exact algorithm
+            fall short even for relatively small grids. This suite provides access to a set of these problems
+            to tackle the problem from an optimization perspective.
+
+            There are two equivalent representations for these problems: Integer-based and Real-valued. 
+
+            For the Real-valued, the goal is to find the a point in [0,1)^d which minimizes the resulting discepancy directly.
+
+            The Integer-valued version takes advantage of the fact that for each dimension, the optimium 
+            has a value matching an existing point int the grid. As such, the search for a point in the grid can 
+            be reduced to finding in each dimension the index of the coordinate, resulting in a search domain 
+            [0,n]^d, where n is the number of gridpoints. 
+
+
+            Reference
+            ---------
+            [Clement23] Clement, Francois, Diederick Vermetten, Jacob de Nobel, Alexandre Jesus, 
+            Luís Paquete, and Carola Doerr. "Computing Star Discrepancies with 
+            Numerical Black-Box Optimization Algorithms."
+
+        )pbdoc")
         .def(py::init(&star_discrepancy_init<T>), py::arg("instance") = 1, py::arg("n_variables") = 5,
              py::arg("n_samples") = 5, py::arg("sampler_type") = SamplerType::UNIFORM,
              R"pbdoc(
@@ -1196,7 +1339,8 @@ void define_star_discrepancy_problems(py::module &m)
     using namespace ioh::problem::star_discrepancy;
     using namespace ioh::common::random::sampler;
 
-    py::enum_<SamplerType>(m, "StarDiscrepancySampler")
+    py::enum_<SamplerType>(m, "StarDiscrepancySampler", 
+        "Methods which can be used to sample the initial grids for the star discrepancy problems")
         .value("UNIFORM", SamplerType::UNIFORM)
         .value("HALTON", SamplerType::HALTON)
         .value("SOBOL", SamplerType::SOBOL)
