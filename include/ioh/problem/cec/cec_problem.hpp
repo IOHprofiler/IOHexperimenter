d647fe507c50c7e82083c1e78c654b43c2274577
Dimitri Rusin <dimitri@habimm.com>
2023-08-09 18:36:40 +0200

    Don't use Eigen, implement a C++ native matrix multiplication function

--- include/ioh/problem/cec/cec_problem.hpp
+++ include/ioh/problem/cec/cec_problem.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <Eigen/Dense>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -16,8 +15,13 @@ namespace ioh::problem
     class CEC : public RealSingleObjective
     {
     public:
+
+        int objective_shift_;
+        std::vector<double> variables_shift_;
+        std::vector<std::vector<double>> linear_transformation_;
+
         /**
-         * @brief Construct a new CEC object
+         * @brief Construct a new CEC object.
          *
          * @param problem_id The id of the problem (should be unique)
          * @param instance The instance of the problem (ignored for cec for now)
@@ -26,7 +30,6 @@ namespace ioh::problem
          *                 since we take all transformation data from static files
          * @param n_variables the dimension of the problem (the size of the search space, how many x varables)
          * @param name the name of the problem (should be unique)
-         * @param path the transformation file (just an example, maybe you want to do this differently)
          */
         CEC
         (
@@ -37,47 +40,161 @@ namespace ioh::problem
         ) : RealSingleObjective
             (
                 MetaData(problem_id, instance, name, n_variables),
-                Bounds<double>(n_variables, -5, 5)
+                Bounds<double>(n_variables, -100, 100)
             )
         {
+            // The only reason we allow 1 dimension is
+            // because of code in line include/ioh/common/factory.hpp:207
+            if (n_variables == 1) { return; }
+
+            if (n_variables != 2 && n_variables != 10 && n_variables != 20)
+            {
+                std::ostringstream message;
+                message << "Error: n_variables should be 2, 10, or 20. Provided n_variables: " << n_variables;
+                throw std::invalid_argument(message.str());
+            }
+
+            this->load_transformation_data();
+
+            // Copy the from-a-static-file-loaded variables shift into the Problem.Solution.optimum_ attribute.
+            this->optimum_.x = this->variables_shift_;
+
+            std::cout << "========================" << std::endl;
+            std::cout << "Objective Shift: " << this->objective_shift_ << std::endl;
+            std::cout << "Linear Transformation Matrix: " << std::endl;
+            for (const auto& row : this->linear_transformation_)
+            {
+                for (double val : row)
+                {
+                    std::cout << val << " ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << "Variables Shift: ";
+            for (double value : this->variables_shift_)
+            {
+                std::cout << value << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "========================" << std::endl;
+        }
+
+        std::vector<double> basic_transform(std::vector<double>& x, double factor, double addend)
+        {
+            std::cout << "Input Vector (x): ";
+            for (double val : x) {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
+
+            // Subtract x and variables_shift_
+            std::vector<double> transformed(x.size());
+            for (size_t i = 0; i < x.size(); i++) {
+                transformed[i] = x[i] - variables_shift_[i];
+            }
+
+            std::cout << "After subtraction (x - variables_shift_): ";
+            for (double val : transformed) {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
+
+            // Scale the transformed vector
+            for (size_t i = 0; i < transformed.size(); i++) {
+                transformed[i] *= factor;
+            }
+
+            std::cout << "After scaling (* " << factor << " ): ";
+            for (double val : transformed) {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
+
+            if (!transformed.empty()) {
+                transformed = this->matVecMultiply(linear_transformation_, transformed);
+                std::cout << "After matrix transformation: ";
+                for (double val : transformed) {
+                    std::cout << val << " ";
+                }
+                std::cout << std::endl;
+            }
+
+            // Add 1 to every element
+            for (double& val : transformed) {
+                val += addend;
+            }
+
+            std::cout << "After adding " << addend << " to every element: ";
+            for (double val : transformed) {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
+
+            std::cout << "Final Transformed Vector: ";
+            for (double val : transformed) {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
+
+            return transformed;
+        }
+
+        double transform_objectives(const double y) override
+        {
+            std::cout << "Objective shift: " << this->objective_shift_ << std::endl;
+
+            auto&& transformed = y + this->objective_shift_;
+            std::cout << "Transformed objective: " << transformed << std::endl;
+
+            return transformed;
+        }
+
+        // Matrix-vector multiplication function
+        std::vector<double> matVecMultiply(const std::vector<std::vector<double>>& mat, const std::vector<double>& vec)
+        {
+            int rows = mat.size();
+            int cols = vec.size();
+            std::vector<double> result(rows, 0.0);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i] += mat[i][j] * vec[j];
+                }
+            }
+            return result;
         }
 
         //! Handler for reading all static data
-        void load_transformation_data
-        (
-            int& objective_shift,
-            Eigen::MatrixXd& linear_transformation,
-            std::vector<double>& variables_shift,
-            const int cec_function_identifier,
-            const int n_variables
-        )
+        void load_transformation_data()
         {
+            const int n_variables = this->meta_data_.n_variables;
+            const int cec_function_identifier = this->meta_data_.problem_id;
+
             // Load F_i_star.
             std::ostringstream F_i_star_filepathStream;
             F_i_star_filepathStream << "include/ioh/problem/cec/input_data/F_i_star.txt";
             std::string F_i_star_filepath = F_i_star_filepathStream.str();
-            load_objective_shift(objective_shift, F_i_star_filepath, cec_function_identifier);
+            load_objective_shift(F_i_star_filepath);
 
             // Load M.
             std::ostringstream M_filepathStream;
             M_filepathStream << "include/ioh/problem/cec/input_data/M_" << cec_function_identifier << "_D" << n_variables << ".txt";
             std::string M_filepath = M_filepathStream.str();
-            load_linear_transformation(linear_transformation, M_filepath);
+            load_linear_transformation(linear_transformation_, M_filepath);
 
             // Load o.
             std::ostringstream shift_data_filepathStream;
             shift_data_filepathStream << "include/ioh/problem/cec/input_data/shift_data_" << cec_function_identifier << ".txt";
             std::string shift_data_filepath = shift_data_filepathStream.str();
-            load_variables_shift(variables_shift, shift_data_filepath, n_variables);
+            load_variables_shift(variables_shift_, shift_data_filepath);
         }
 
-        void load_objective_shift
-        (
-            int& objective_shift,
-            const std::string& F_i_star_filepath,
-            const int cec_function_identifier
-        )
+        void load_objective_shift(const std::string& F_i_star_filepath)
         {
+            const int cec_function_identifier = this->meta_data_.problem_id;
+
             std::ifstream file(F_i_star_filepath); // Open the file
             if (!file.is_open()) {
                 std::cerr << "Failed to open the file: " << F_i_star_filepath << std::endl;
@@ -96,14 +213,14 @@ namespace ioh::problem
             }
 
             if (cec_function_identifier > 0 && cec_function_identifier <= shifts.size()) {
-                objective_shift = shifts[cec_function_identifier - 1];
+                this->objective_shift_ = shifts[cec_function_identifier - 1];
                 return;
             }
 
-            throw std::out_of_range("CEC Function identifier out of range.");
+            throw std::out_of_range("CEC function identifier out of range.");
         }
 
-        void load_linear_transformation(Eigen::MatrixXd& linear_transformation, const std::string& M_filepath)
+        void load_linear_transformation(std::vector<std::vector<double>>& linear_transformation_, const std::string& M_filepath)
         {
             std::ifstream file(M_filepath); // Open the file
 
@@ -113,7 +230,6 @@ namespace ioh::problem
                 return;
             }
 
-            std::vector<std::vector<double>> data;
             std::string line;
 
             // Read each line
@@ -129,27 +245,14 @@ namespace ioh::problem
                     row.push_back(value);
                 }
 
-                data.push_back(row);
-            }
-
-            // Initialize the Eigen matrix with the data
-            linear_transformation.resize(data.size(), data[0].size());
-            for (size_t i = 0; i < data.size(); i++)
-            {
-                for (size_t j = 0; j < data[i].size(); j++)
-                {
-                    linear_transformation(i, j) = data[i][j];
-                }
+                linear_transformation_.push_back(row);
             }
         }
 
-        void load_variables_shift
-        (
-            std::vector<double>& variables_shift,
-            const std::string& shift_data_filepath,
-            const int n_variables
-        )
+        void load_variables_shift(std::vector<double>& variables_shift_, const std::string& shift_data_filepath)
         {
+            const int n_variables = this->meta_data_.n_variables;
+
             std::ifstream file(shift_data_filepath); // Open the file
 
             if (!file.is_open()) {
@@ -165,7 +268,7 @@ namespace ioh::problem
 
                 int count = 0;
                 while (ss >> value && count < n_variables) { // Parse the tokens and convert to double
-                    variables_shift.push_back(value);
+                    variables_shift_.push_back(value);
                     count++;
                 }
             }
