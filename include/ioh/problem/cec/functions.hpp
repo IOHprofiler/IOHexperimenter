0e01d97f18a146c2f548d12c84e0fb108a82f25c
Dimitri Rusin <dimitri@habimm.com>
2023-08-14 17:48:31 +0200

    Add 4 composite functions

--- include/ioh/problem/cec/functions.hpp
+++ include/ioh/problem/cec/functions.hpp
@@ -1,73 +1,83 @@
 #pragma once
 
+#include "cec_problem.hpp"
+
 namespace ioh::problem::cec
 {
+    #define INF 1.0e99
+    #define EPS 1.0e-14
     #define E  2.7182818284590452353602874713526625
     #define PI 3.1415926535897932384626433832795029
 
-    void shiftfunc (const double *x, double *xshift, int nx,double *Os)
+    void shiftfunc (const double *x, double *xshift, int nx, double *Os)
     {
-        int i;
-        for (i=0; i<nx; i++)
+        for (int i = 0; i < nx; i++)
         {
-            xshift[i]=x[i]-Os[i];
+            xshift[i] = x[i] - Os[i];
+            LOG("shiftfunc, i: " << i << ", xshift[" << i << "]: " << xshift[i]);
         }
     }
 
-    void rotatefunc (const double *x, double *xrot, int nx,double *Mr)
+    void rotatefunc (const double *x, double *xrot, int nx, double *Mr)
     {
-        int i,j;
-        for (i=0; i<nx; i++)
+        for (int i = 0; i < nx; i++)
         {
-            xrot[i]=0;
-                for (j=0; j<nx; j++)
-                {
-                    xrot[i]=xrot[i]+x[j]*Mr[i*nx+j];
-                }
+            xrot[i] = 0;
+            for (int j = 0; j < nx; j++)
+            {
+                xrot[i] = xrot[i] + x[j] * Mr[i*nx + j];
+            }
+            LOG("rotatefunc, i: " << i << ", xrot[" << i << "]: " << xrot[i]);
         }
     }
 
-    void sr_func (const double *x, double *sr_x, int nx, double *Os,double *Mr, double sh_rate, int s_flag,int r_flag) /* shift and rotate */
+    void sr_func (const double *x, double *sr_x, int nx, double *Os, double *Mr, double sh_rate, int s_flag, int r_flag)
     {
         double* y = (double *)malloc(sizeof(double) * nx);
-        int i;
-        if (s_flag==1)
+
+        if (s_flag == 1)
         {
-            if (r_flag==1)
+            if (r_flag == 1)
             {
                 shiftfunc(x, y, nx, Os);
-                for (i=0; i<nx; i++)//shrink to the orginal search range
+                for (int i = 0; i < nx; i++)
                 {
-                    y[i]=y[i]*sh_rate;
+                    y[i] = y[i] * sh_rate;
+                    LOG("sr_func (s_flag==1 && r_flag==1), i: " << i << ", y[" << i << "]: " << y[i]);
                 }
                 rotatefunc(y, sr_x, nx, Mr);
             }
             else
             {
                 shiftfunc(x, sr_x, nx, Os);
-                for (i=0; i<nx; i++)//shrink to the orginal search range
+                for (int i = 0; i < nx; i++)
                 {
-                    sr_x[i]=sr_x[i]*sh_rate;
+                    sr_x[i] = sr_x[i] * sh_rate;
+                    LOG("sr_func (s_flag==1 && r_flag!=1), i: " << i << ", sr_x[" << i << "]: " << sr_x[i]);
                 }
             }
         }
         else
         {
-
-            if (r_flag==1)
+            if (r_flag == 1)
             {
-                for (i=0; i<nx; i++)//shrink to the orginal search range
+                for (int i = 0; i < nx; i++)
                 {
-                    y[i]=x[i]*sh_rate;
+                    y[i] = x[i] * sh_rate;
+                    LOG("sr_func (s_flag!=1 && r_flag==1), i: " << i << ", y[" << i << "]: " << y[i]);
                 }
                 rotatefunc(y, sr_x, nx, Mr);
             }
             else
-            for (i=0; i<nx; i++)//shrink to the orginal search range
             {
-                sr_x[i]=x[i]*sh_rate;
+                for (int i = 0; i < nx; i++)
+                {
+                    sr_x[i] = x[i] * sh_rate;
+                    LOG("sr_func (s_flag!=1 && r_flag!=1), i: " << i << ", sr_x[" << i << "]: " << sr_x[i]);
+                }
             }
         }
+
         free(y);
     }
 
@@ -76,15 +86,23 @@ namespace ioh::problem::cec
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
         sr_func (x, z, nx, Os, Mr,1.0, s_flag, r_flag); /* shift and rotate */
-        f[0] = z[0]*z[0];
+        f[0] = z[0] * z[0];
+        LOG("bent_cigar_func f[0] (using z[0]): " << f[0]);  // Print initial f[0] value
+        LOG("Initial f[0] (using z[0]): " << f[0]);  // Print initial f[0] value
         for (i=1; i<nx; i++)
         {
-            f[0] += pow(10.0,6.0)*z[i]*z[i];
+            LOG("Iteration: " << i);        // Print current iteration number
+            LOG("z[" << i << "]: " << z[i]); // Print current z value
+            LOG("x[" << i << "]: " << x[i]); // Print current x value
+
+            f[0] += pow(10.0, 6.0) * z[i] * z[i];
+
+            LOG("f[0] after iteration " << i << ": " << f[0]);  // Print current f[0] value
         }
         free(z);
     }
 
-    void hgbat_func (double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    void hgbat_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
@@ -120,7 +138,7 @@ namespace ioh::problem::cec
         free(z);
     }
 
-    void rastrigin_func (double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    void rastrigin_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
@@ -135,7 +153,7 @@ namespace ioh::problem::cec
         free(z);
     }
 
-    void katsuura_func (double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    void katsuura_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i,j;
@@ -161,7 +179,7 @@ namespace ioh::problem::cec
         free(z);
     }
 
-    void happycat_func (double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    void happycat_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
@@ -182,7 +200,7 @@ namespace ioh::problem::cec
         free(z);
     }
 
-    void grie_rosen_func (double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    void grie_rosen_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
@@ -207,7 +225,7 @@ namespace ioh::problem::cec
         free(z);
     }
 
-    void schwefel_func (double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag) /* Schwefel's  */
+    void schwefel_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag) /* Schwefel's  */
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
@@ -238,7 +256,7 @@ namespace ioh::problem::cec
         free(z);
     }
 
-    void ackley_func (double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    void ackley_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
@@ -260,7 +278,7 @@ namespace ioh::problem::cec
     }
 
     // y is the x, permuted by S; this y value is used in computing Hybrid Function 2
-    void schaffer_F7_func (double *x, double *f, double *y, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    void schaffer_F7_func (const double *x, double *f, double *y, int nx, double *Os,double *Mr,int s_flag, int r_flag)
     {
         double* z=(double *)malloc(sizeof(double)  *  nx);
         int i;
@@ -275,5 +293,231 @@ namespace ioh::problem::cec
         }
         f[0] = f[0]*f[0]/(nx-1)/(nx-1);
         free(z);
+    }
+
+    void cf_cal(const double *x, double *f, int nx, double *Os,double * delta,double * bias,double * fit, int cf_num)
+    {
+        int i,j;
+        double *w;
+        double w_max=0,w_sum=0;
+        w=(double *)malloc(cf_num * sizeof(double));
+        for (i=0; i<cf_num; i++)
+        {
+            fit[i]+=bias[i];
+            w[i]=0;
+            for (j=0; j<nx; j++)
+            {
+                w[i]+=pow(x[j]-Os[i*nx+j],2.0);
+            }
+            if (w[i]!=0)
+                w[i]=pow(1.0/w[i],0.5)*exp(-w[i]/2.0/nx/pow(delta[i],2.0));
+            else
+                w[i]=INF;
+            if (w[i]>w_max)
+                w_max=w[i];
+        }
+
+        for (i=0; i<cf_num; i++)
+        {
+            w_sum=w_sum+w[i];
+        }
+        if(w_max==0)
+        {
+            for (i=0; i<cf_num; i++)
+                w[i]=1;
+            w_sum=cf_num;
+        }
+        f[0] = 0.0;
+        for (i=0; i<cf_num; i++)
+        {
+            LOG("Iteration: " << i);         // Print current iteration number
+            LOG("w[" << i << "]: " << w[i]); // Print current w value
+            LOG("fit[" << i << "]: " << fit[i]); // Print current fit value
+
+            f[0] = f[0] + w[i] / w_sum * fit[i];
+
+            LOG("f[0] after iteration " << i << ": " << f[0]); // Print current f[0] value
+        }
+        free(w);
+    }
+
+    void rosenbrock_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag) /* Rosenbrock's */
+    {
+        double* z=(double *)malloc(sizeof(double)  *  nx);
+        int i;
+        double tmp1,tmp2;
+        f[0] = 0.0;
+        sr_func (x, z, nx, Os, Mr, 2.048/100.0, s_flag, r_flag); /* shift and rotate */
+        z[0] += 1.0;//shift to orgin
+        for (i=0; i<nx-1; i++)
+        {
+            z[i+1] += 1.0;//shift to orgin
+            tmp1=z[i]*z[i]-z[i+1];
+            tmp2=z[i]-1.0;
+            f[0] += 100.0*tmp1*tmp1 +tmp2*tmp2;
+        }
+        free(z);
+    }
+
+    void ellips_func (const double *x, double *f, int nx, double *Os,double *Mr, int s_flag, int r_flag) /* Ellipsoidal */
+    {
+        double* z=(double *)malloc(sizeof(double)  *  nx);
+        int i;
+        f[0] = 0.0;
+        sr_func (x, z, nx, Os, Mr,1.0, s_flag, r_flag); /* shift and rotate */
+        for (i=0; i<nx; i++)
+        {
+           f[0] += pow(10.0,6.0*i/(nx-1))*z[i]*z[i];
+        }
+        free(z);
+    }
+
+    void discus_func (const double *x, double *f, int nx, double *Os,double *Mr, int s_flag, int r_flag) /* Discus */
+    {
+        double* z=(double *)malloc(sizeof(double)  *  nx);
+        int i;
+        sr_func (x, z, nx, Os, Mr,1.0, s_flag, r_flag); /* shift and rotate */
+        f[0] = pow(10.0,6.0)*z[0]*z[0];
+        for (i=1; i<nx; i++)
+        {
+            f[0] += z[i]*z[i];
+        }
+        free(z);
+    }
+
+    void escaffer6_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    {
+        double* z=(double *)malloc(sizeof(double)  *  nx);
+        int i;
+        double temp1, temp2;
+
+        sr_func (x, z, nx, Os, Mr, 1.0, s_flag, r_flag); /* shift and rotate */
+
+        f[0] = 0.0;
+        for (i=0; i<nx-1; i++)
+        {
+            LOG("escaffer6_func loop, i: " << i << ", z[" << i << "]: " << z[i]);  // LOGGING
+
+            temp1 = sin(sqrt(z[i]*z[i]+z[i+1]*z[i+1]));
+            temp1 =temp1*temp1;
+            temp2 = 1.0 + 0.001*(z[i]*z[i]+z[i+1]*z[i+1]);
+            f[0] += 0.5 + (temp1-0.5)/(temp2*temp2);
+        }
+        temp1 = sin(sqrt(z[nx-1]*z[nx-1]+z[0]*z[0]));
+        temp1 =temp1*temp1;
+        temp2 = 1.0 + 0.001*(z[nx-1]*z[nx-1]+z[0]*z[0]);
+        f[0] += 0.5 + (temp1-0.5)/(temp2*temp2);
+        free(z);
+    }
+
+    void griewank_func (const double *x, double *f, int nx, double *Os,double *Mr,int s_flag, int r_flag)
+    {
+        double* z=(double *)malloc(sizeof(double)  *  nx);
+        int i;
+        double s, p;
+        s = 0.0;
+        p = 1.0;
+
+        sr_func (x, z, nx, Os, Mr, 600.0/100.0, s_flag, r_flag); /* shift and rotate */
+
+        for (i=0; i<nx; i++)
+        {
+            LOG("griewank_func loop, i: " << i << ", z[" << i << "]: " << z[i]);  // LOGGING
+
+            s += z[i]*z[i];
+            p *= cos(z[i]/sqrt(1.0+i));
+        }
+        f[0] = 1.0 + s/4000.0 - p;
+        free(z);
+    }
+
+    void cf01 (const double *x, double *f, int nx, double *Os,double *Mr,int r_flag) /* Composition Function 1 */
+    {
+        int i,cf_num=5;
+        double fit[5];
+        double delta[5] = {10, 20, 30, 40, 50};
+        double bias[5] = {0, 200, 300, 100, 400};
+
+        i=0;
+        rosenbrock_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+4;
+        i=1;
+        ellips_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+10;
+        i=2;
+        bent_cigar_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+30;
+        i=3;
+        discus_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+10;
+        i=4;
+        ellips_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,0);
+        fit[i]=10000*fit[i]/1e+10;
+        cf_cal(x, f, nx, Os, delta,bias,fit,cf_num);
+    }
+
+    void cf02 (const double *x, double *f, int nx, double *Os,double *Mr,int r_flag) /* Composition Function 2 */
+    {
+        int i,cf_num=3;
+        double fit[3];
+        double delta[3] = {20,10,10};
+        double bias[3] = {0, 200, 100};
+
+        i=0;
+        schwefel_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,0);
+        i=1;
+        rastrigin_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        i=2;
+        hgbat_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        cf_cal(x, f, nx, Os, delta,bias,fit,cf_num);
+    }
+
+    void cf06 (const double *x, double *f, int nx, double *Os,double *Mr,int r_flag) /* Composition Function 4 */
+    {
+        int i,cf_num=5;
+        double fit[5];
+        double delta[5] = {20,20,30,30,20};
+        double bias[5] = {0, 200, 300, 400, 200};
+        i=0;
+        escaffer6_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/2e+7;
+        i=1;
+        schwefel_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        i=2;
+        griewank_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=1000*fit[i]/100;
+        i=3;
+        rosenbrock_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        i=4;
+        rastrigin_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+3;
+        cf_cal(x, f, nx, Os, delta,bias,fit,cf_num);
+    }
+
+    void cf07 (const double *x, double *f, int nx, double *Os,double *Mr,int r_flag) /* Composition Function 4 */
+    {
+        int i,cf_num=6;
+        double fit[6];
+        double delta[6] = {10,20,30,40,50,60};
+        double bias[6] = {0, 300, 500, 100, 400, 200};
+        i=0;
+        hgbat_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1000;
+        i=1;
+        rastrigin_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+3;
+        i=2;
+        schwefel_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/4e+3;
+        i=3;
+        bent_cigar_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+30;
+        i=4;
+        ellips_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/1e+10;
+        i=5;
+        escaffer6_func(x,&fit[i],nx,&Os[i*nx],&Mr[i*nx*nx],1,r_flag);
+        fit[i]=10000*fit[i]/2e+7;
+        cf_cal(x, f, nx, Os, delta,bias,fit,cf_num);
     }
 } // namespace ioh::problem::cec
