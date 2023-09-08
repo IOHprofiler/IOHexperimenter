7a360783d6bf0e07f72fbf45d0917dd1a4261b7e
Dimitri Rusin <dimitri@habimm.com>
2023-09-08 17:10:09 +0200

    Quit if a Hybrid Functions is initialized for dimension 2.

--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ setup(
     description="The experimenter for Iterative Optimization Heuristics",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=find_packages(include=["ioh", "ioh.*"]),
     package_dir={"IOHexperimenter": "ioh"},
     include_package_data=True,
     package_data={
