1633b1cdd22f0197a718f7727f23b8e55753202c
Dimitri Rusin <dimitri@habimm.com>
2023-11-10 15:22:53 +0100

    reorder some lines

--- include/ioh/problem/dynamic_bin_val/dynamic_bin_val_pareto.hpp
+++ include/ioh/problem/dynamic_bin_val/dynamic_bin_val_pareto.hpp
@@ -37,11 +37,11 @@ namespace ioh::problem
     {
     public:
 
-        int timestep;                   /**< The current timestep in the dynamic binary value problem scenario. */
-        std::vector<unsigned long> weights;       /**< A vector of weights used in the evaluation of the problem. */
-        std::default_random_engine random_generator;
+        double pareto_shape;                            // Alpha parameter for the Pareto distribution
         double pareto_upper_bound;
-        double pareto_shape; // Alpha parameter for the Pareto distribution
+        int timestep;                                   /**< The current timestep in the dynamic binary value problem scenario. */
+        std::default_random_engine random_generator;
+        std::vector<unsigned long> weights;             /**< A vector of weights used in the evaluation of the problem. */
 
         /**
          * @brief Constructs a new instance of DynamicBinValPareto.
@@ -55,33 +55,25 @@ namespace ioh::problem
                 MetaData(10003, instance, "DynamicBinValPareto", n_variables, common::OptimizationType::MAX),
                 Bounds<int>(n_variables, 0, 1)
             ),
-            random_generator(instance),
             pareto_shape(0.1),
-            pareto_upper_bound(static_cast<double>(std::numeric_limits<unsigned long>::max()) / static_cast<double>(n_variables))
+            pareto_upper_bound(static_cast<double>(std::numeric_limits<unsigned long>::max()) / static_cast<double>(n_variables)),
+            timestep(0),
+            random_generator(instance)
         {
             if (n_variables == 1) { return; }
 
-            this->timestep = 0;
             this->weights.resize(n_variables);
 
             std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-            for(size_t i = 0; i < this->weights.size(); ++i) {
+            for(size_t i = 0; i < this->weights.size(); ++i)
+            {
                 double uniform_sample = distribution(this->random_generator);
 
                 // Calculate the weight using the power-law distribution inversion formula
                 // Truncate the distribution to prevent overflow when weights are summed
                 auto pareto_distributed = std::min(std::pow(1.0 - uniform_sample, -1.0 / pareto_shape), pareto_upper_bound);
                 this->weights[i] = static_cast<unsigned long>(pareto_distributed);
-                if (this->weights[i] == 0)
-                {
-                    std::string error_message = "We sampled a 0 from a Pareto distribution at index " + std::to_string(i);
-                    error_message += " with uniform_sample = " + std::to_string(uniform_sample);
-                    error_message += ", pareto_distributed = " + std::to_string(pareto_distributed);
-                    error_message += ", pareto_shape = " + std::to_string(pareto_shape);
-                    error_message += ", pareto_upper_bound = " + std::to_string(pareto_upper_bound);
-                    throw std::runtime_error(error_message);
-                }
             }
         }
 
@@ -91,22 +83,14 @@ namespace ioh::problem
 
             std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-            for(size_t i = 0; i < this->weights.size(); ++i) {
+            for(size_t i = 0; i < this->weights.size(); ++i)
+            {
                 double uniform_sample = distribution(this->random_generator);
 
                 // Calculate the weight using the power-law distribution inversion formula
                 // Truncate the distribution to prevent overflow when weights are summed
                 auto pareto_distributed = std::min(std::pow(1.0 - uniform_sample, -1.0 / pareto_shape), pareto_upper_bound);
                 this->weights[i] = static_cast<unsigned long>(pareto_distributed);
-                if (this->weights[i] == 0)
-                {
-                    std::string error_message = "We sampled a 0 from a Pareto distribution at index " + std::to_string(i);
-                    error_message += " with uniform_sample = " + std::to_string(uniform_sample);
-                    error_message += ", pareto_distributed = " + std::to_string(pareto_distributed);
-                    error_message += ", pareto_shape = " + std::to_string(pareto_shape);
-                    error_message += ", pareto_upper_bound = " + std::to_string(pareto_upper_bound);
-                    throw std::runtime_error(error_message);
-                }
             }
 
             return this->timestep;
