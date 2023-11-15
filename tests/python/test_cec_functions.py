1c388415a30a4bc5eb1e91bb437b79f38610578d
Dimitri Rusin <dimitri@habimm.com>
2023-11-15 11:11:26 +0100

    Output which problems are defined, in the Python script.

--- tests/python/test_cec_functions.py
+++ tests/python/test_cec_functions.py
@@ -1,6 +1,9 @@
-from ioh import get_problem
+import ioh
 
-f1 = get_problem(1001, 1, 2)
+print("ioh.problem.RealSingleObjective.problems")
+print(ioh.problem.RealSingleObjective.problems)
+
+f1 = ioh.get_problem(1001, 1, 2)
 x1 = [2, 31]
 y1 = f1(x1)
 print()
@@ -8,7 +11,7 @@ print("function 1001")
 print(x1)
 print(y1)
 
-f2 = get_problem(1002, 1, 2)
+f2 = ioh.get_problem(1002, 1, 2)
 x2 = [2, 31]
 y2 = f2(x2)
 print()
@@ -16,7 +19,7 @@ print("function 1002")
 print(x2)
 print(y2)
 
-f3 = get_problem(1003, 1, 2)
+f3 = ioh.get_problem(1003, 1, 2)
 x3 = [2, 31]
 y3 = f3(x3)
 print()
@@ -24,7 +27,7 @@ print("function 1003")
 print(x3)
 print(y3)
 
-f4 = get_problem(1004, 1, 2)
+f4 = ioh.get_problem(1004, 1, 2)
 x4 = [2, 31]
 y4 = f4(x4)
 print()
@@ -32,7 +35,7 @@ print("function 1004")
 print(x4)
 print(y4)
 
-f = get_problem(1005, 1, 2)
+f = ioh.get_problem(1005, 1, 2)
 x = [2, 31]
 y = f(x)
 print()
@@ -40,7 +43,7 @@ print("function 1005")
 print(x)
 print(y)
 
-f = get_problem(1006, 1, 20)
+f = ioh.get_problem(1006, 1, 20)
 x = [1,2,5,3,22,2,2,7,6,5,5,4,34,1,2,-13,4,5,6,7]
 y = f(x)
 print()
@@ -48,7 +51,7 @@ print("function 1006")
 print(x)
 print(y)
 
-f = get_problem(1007, 1, 20)
+f = ioh.get_problem(1007, 1, 20)
 x = [1,2,5,3,22,2,2,7,6,5,5,4,34,1,2,-13,4,5,6,7]
 y = f(x)
 print()
@@ -56,7 +59,7 @@ print("function 1007")
 print(x)
 print(y)
 
-f = get_problem(1008, 1, 20)
+f = ioh.get_problem(1008, 1, 20)
 x = [1,2,5,3,22,2,2,7,6,5,5,4,34,1,2,-13,4,5,6,7]
 y = f(x)
 print()
@@ -64,7 +67,7 @@ print("function 1008")
 print(x)
 print(y)
 
-f = get_problem(1009, 1, 2)
+f = ioh.get_problem(1009, 1, 2)
 x = [2, 31]
 y = f(x)
 print()
@@ -72,7 +75,7 @@ print("function 1009")
 print(x)
 print(y)
 
-f = get_problem(1010, 1, 2)
+f = ioh.get_problem(1010, 1, 2)
 x = [2, 31]
 y = f(x)
 print()
@@ -80,7 +83,7 @@ print("function 1010")
 print(x)
 print(y)
 
-f = get_problem(1011, 1, 2)
+f = ioh.get_problem(1011, 1, 2)
 x = [2, 31]
 y = f(x)
 print()
@@ -88,7 +91,7 @@ print("function 1011")
 print(x)
 print(y)
 
-f = get_problem(1012, 1, 2)
+f = ioh.get_problem(1012, 1, 2)
 x = [2, 31]
 y = f(x)
 print()
