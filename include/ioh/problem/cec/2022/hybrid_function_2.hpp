88b85fdd765052478da22f649afb707195f5920d
Jacob de Nobel <jacobdenobel@gmail.com>
2024-02-12 18:58:53 +0100

    fix hf2

--- include/ioh/problem/cec/2022/hybrid_function_2.hpp
+++ include/ioh/problem/cec/2022/hybrid_function_2.hpp
@@ -29,84 +29,31 @@ namespace ioh::problem::cec2022
         /// \return The transformed variables.
         std::vector<double> transform_variables(std::vector<double> x) override
         {
-
             std::vector<double> z(x.size());
             std::vector<double> y(x.size());
 
             transformation::variables::scale_and_rotate(x, z, y, this->variables_shifts_[0],
                                                         this->linear_transformations_[0], 1.0, true, true);
 
-            for (size_t i = 0; i < x.size(); ++i)
+            for (size_t i = 0; i < x.size(); i++)
             {
                 y[i] = z[this->input_permutation_[i] - 1];
             }
+            
+            const int n = static_cast<int>(x.size());
+            int n_shaffer = n;
+            for (const auto gp : {0.1, 0.2, 0.2, 0.2, 0.1})
+            {
+                n_shaffer -= static_cast<int>(std::ceil(gp * n));
+            }
+
+            for (int i = 0; i < n_shaffer; i++)
+            {
+                y[y.size() - n_shaffer + i] = y[i];
+            }
+
             return y;
-
-            // auto &&Os = this->variables_shifts_[0];
-            // auto &&Mr = this->linear_transformations_[0];
-            // auto &&S = this->input_permutation_;
-            //
-            // constexpr int cf_num = 6;
-            // std::vector<double> z(x.size());
-            // std::vector<double> y(x.size());
-            // std::vector<int> G_nx(cf_num);
-            // std::vector<int> G(cf_num);
-            // std::vector<double> Gp = {0.1, 0.2, 0.2, 0.2, 0.1, 0.2};
-            // int nx = x.size();
-            //
-            // int tmp = 0;
-            // for (int i = 0; i < cf_num - 1; i++)
-            // {
-            //     G_nx[i] = static_cast<int>(std::ceil(Gp[i] * nx));
-            //     tmp += G_nx[i];
-            // }
-            // G_nx[cf_num - 1] = nx - tmp;
-            //
-            // G[0] = 0;
-            // for (int i = 1; i < cf_num; i++)
-            // {
-            //     G[i] = G[i - 1] + G_nx[i - 1];
-            // }
-            //
-            // transformation::variables::scale_and_rotate(x, z, y, Os, Mr, 1.0, true, true);
-            //
-            // for (int i = 0; i < nx; i++)
-            // {
-            //     y[i] = z[S[i] - 1];
-            // }
-            // std::vector<double> hgbat_x(y.begin() + G[0], y.begin() + G[0] + G_nx[0]);
-            // std::vector<double> katsuura_x(y.begin() + G[1],
-            //                                y.begin() + G[1] + G_nx[1]);
-            //
-            //
-            // std::vector<double> ackley_x(y.begin() + G[2],
-            //                              y.begin() + G[2] + G_nx[2]);
-            //
-            // std::vector<double> rastrigin_x(y.begin() + G[3], y.begin() + G[3] + G_nx[3]);            
-            //
-            //
-            // std::vector<double> schwefel_x(y.begin() + G[4],
-            //                                y.begin() + G[4] + G_nx[4]);
-            //
-            // std::vector<double> schaffer_y(y.begin(), y.begin() + G_nx[5]);
-            //
-            // std::vector<double> prepared_y;
-            //
-            // // Start by reserving space for efficiency
-            // prepared_y.reserve(x.size());
-            //
-            // prepared_y.insert(prepared_y.end(), hgbat_x.begin(), hgbat_x.end());
-            // prepared_y.insert(prepared_y.end(), katsuura_x.begin(), katsuura_x.end());
-            // prepared_y.insert(prepared_y.end(), ackley_x.begin(), ackley_x.end());
-            // prepared_y.insert(prepared_y.end(), rastrigin_x.begin(), rastrigin_x.end());
-            // prepared_y.insert(prepared_y.end(), schwefel_x.begin(), schwefel_x.end());
-            // prepared_y.insert(prepared_y.end(), schaffer_y.begin(), schaffer_y.end());
-
-            // common::print(y);
-            // common::print(prepared_y);
-            // std::cout << G_nx[5] << std::endl;
-            //
-            // return prepared_y;
+            
         }
 
     public:
