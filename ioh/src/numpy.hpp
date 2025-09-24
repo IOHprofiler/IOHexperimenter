4603f1d163e90e6e75f8b5c1491630bc7739a50f
jacobdenobel <jacobdenobel@gmail.com>
2025-09-24 13:28:34 +0200

    remove unused numpy function

--- ioh/src/numpy.hpp
+++ ioh/src/numpy.hpp
@@ -9,19 +9,6 @@ namespace py = pybind11;
 
 
 template<typename T>
-py::array_t<T> make_mutable_array(const std::vector<T>& x)
-{
-    auto base = py::capsule(x.data(), [](void*){/* no free */});
-    py::array_t<T> arr(
-        {static_cast<size_t>(x.size())}, 
-        {static_cast<size_t>(sizeof(T))},
-        x.data(),
-        base
-    );
-    return arr;
-}
-
-template<typename T>
 py::array_t<T> make_array(const std::vector<T>& x)
 {
     py::array_t<T> arr(
